cbccc35b9d158ee0218feee606c09e06b6b475a4
Ly Mai Le <lymaile@gmail.com>
2014-09-12 11:56:50 -0700

    Revert "(feat) Flippy images"

--- app/www/modules/buy-browse/controllers.js
+++ app/www/modules/buy-browse/controllers.js
@@ -35,10 +35,6 @@ angular.module('buybrowse', ['buybrowse.services'])
     $state.go('tab.buyconfirmation');
   };
 
-  $scope.onTap = function() {
-    $scope.front = !$scope.front;
-  }
-
   //show them the next item, same as swiping left
   $scope.nextItem = function(){
     $scope.onSwipeLeft();
